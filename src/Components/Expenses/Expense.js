47af0e5a0adfb1c04270b4883702ccd3ea48876d
gaddechiranjeevi <gaddechiru@gmail.com>
2023-02-14 20:11:27 +0530

    Final

--- src/Components/Expenses/Expense.js
+++ src/Components/Expenses/Expense.js
@@ -1,177 +1,169 @@
-import axios from 'axios';
-import { useDispatch, useSelector } from "react-redux";
-import { Fragment, useEffect, useRef, useState } from 'react';
-import classes from './Expense.module.css';
-import { ThemeAction } from '../../store/theme-reducer';
-import { expensAction } from '../../store/expense-reducer';
+import { useState } from "react";
+import classes from './Authentication.module.css';
+import ForgotPassword from "./ForgotPassword";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { useDispatch } from "react-redux";
+import { authAction } from "../../store/auth-reducer";
+import { useHistory } from "react-router-dom";
 
-
-const Expense = () => {
-    const expenseRef = useRef(null);
-    const descriptionRef = useRef('');
-    const categoryRef = useRef('');
-    const [totalExpense, setTotalExpense] = useState(0);
+const Authentication = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    const [isForgot, setIsForgot] = useState(false);
     const dispatch = useDispatch();
+    const history = useHistory();
+    
+    const SignUpHandler = (email, password) => {
+        fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBNKcwJ85YxV00sJT8V4pSH2dMBTCWv77k',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+                    }),
+                    headers:{
+                        "Content-Type": "application/json",
+                    }
+                }
+            )
+            .then((res) => {
+                if(res.ok){
+                  return res.json()
+                }else{
+                  return res.json().then((data) => {
+                    const errormsg = data.error.message;
+                    throw new Error(errormsg)
+                  })
+                }
+              })
+              .then((data) => {
+                console.log('successfully created account');
+                console.log(data);
+              })
+              .catch((err) => {
+                alert(err.message);
+              })
+        }
 
-    const expensesDispatched = useSelector((state) => state.expenses);
-    console.log(expensesDispatched.expenses);
-
-    const getExpenseData = () => {
-        axios.get(
-            'https://expense-tracker-7d5cb-default-rtdb.firebaseio.com/expense.json'
-            ).then((res) => {
-                const data = res.data;
-                let sumOfExpenses = 0;
-                Object.values(data).forEach((item) => {
-                    sumOfExpenses += Number(item.amount)
-                })
-                setTotalExpense(sumOfExpenses);
-                dispatch(expensAction.onAddOrGetExpense(data))
-            }).catch((err) => {
-                console.log(err);
-            })
-    };
-
-    useEffect(getExpenseData, [dispatch]);
-
-    const addExpenseHandler = async(event) => {
-        event.preventDefault();
-
-        const enteredExpense = expenseRef.current.value;
-        const enteredDescription = descriptionRef.current.value;
-        const enteredCategory = categoryRef.current.value;
-
-        const expenseObj = {
-            amount: enteredExpense,
-            description: enteredDescription,
-            category: enteredCategory
+        const LoginHandler = (email, password) => {
+            fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBNKcwJ85YxV00sJT8V4pSH2dMBTCWv77k',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        email: email,
+                        password: password,
+                        returnSecureToken: true
+                    }),
+                    headers:{
+                        "Content-Type": "application/json",
+                    }
+                }
+            )
+            .then((res) => {
+                if(res.ok){
+                  return res.json()
+                }else{
+                  return res.json().then((data) => {
+                    const errormsg = data.error.message;
+                    throw new Error(errormsg)
+                  })
+                }
+              })
+              .then((data) => {
+                const loginObj={idToken: data.idToken, email: data.email}
+                dispatch(authAction.login(loginObj))
+                console.log(data);
+                console.log('successfully loggedIn');
+                history.replace('/home');
+              })
+              .catch((err) => {
+                alert(err.message);
+              })     
         };
 
-        try {
-            const res = await axios.post('https://expense-tracker-7d5cb-default-rtdb.firebaseio.com/expense.json',
-            expenseObj );
-            console.log(res);
-            getExpenseData();
-        } catch (err) {
-            console.log(err);
+        const forgotPasswordHandler = (email) => {
+          fetch(
+              'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBNKcwJ85YxV00sJT8V4pSH2dMBTCWv77k',
+              {
+                  method: 'POST',
+                  body: JSON.stringify({
+                      email: email,
+                      requestType: 'PASSWORD_RESET'
+                  }),
+                  headers:{
+                      "Content-Type": "application/json",
+                  }
+              }
+          )
+          .then((res) => {
+              if(res.ok){
+                return res.json()
+              }else{
+                return res.json().then((data) => {
+                  const errormsg = data.error.message;
+                  throw new Error(errormsg)
+                })
+              }
+            })
+            .then((data) => {
+              console.log(data);
+            })
+            .catch((err) => {
+              alert(err.message);
+            })     
+      };
+
+        const onClickSignUpHandler = () => {
+            setIsLogin(true)
+        };
+
+        const onClickLoginHandler = () => {
+            setIsLogin(false)
+        };
+        const onClickPasswordHandler = () => {
+          setIsForgot(true)
         }
-    };
-    const deleteExpenseHandler = (expenseId) => {
-        axios.delete(
-            `https://expense-tracker-7d5cb-default-rtdb.firebaseio.com/expense/${expenseId}.json`
-        ).then((res) => {
-            console.log(res);
-            getExpenseData();
-        }).catch((err) => {
-            console.log(err);
-        })
-    };
 
-    const editExpenseHandler = (expenseId) => {
-        expenseRef.current.value = expensesDispatched.expenses[expenseId].amount;
-        descriptionRef.current.value = expensesDispatched.expenses[expenseId].description;
-        categoryRef.current.value = expensesDispatched.expenses[expenseId].category;
-        deleteExpenseHandler(expenseId);
-    };
-
-    
     return (
-        <Fragment>
-            <form className={classes.expense}onSubmit={addExpenseHandler}>
-                <h1>Add Expense</h1>
-                <div className={classes.amount}>
-                <input
-                    name='expense'
-                    type='number'
-                    placeholder='Enter your Expense'
-                    ref={expenseRef}
-                    required
-                />
-                </div>
-                <div className={classes.desc}>
-                <input
-                    name='description'
-                    type='description'
-                    placeholder='Description'
-                    ref={descriptionRef}
-                    required
-                />
-                </div>
-                <div className={classes.select}>
-                <select
-                    placeholder='Category'
-                    ref={categoryRef}
-                    required >
-                    <option selected>Select Category</option>
-                    <option>Food</option>
-                    <option>Petrol</option>
-                    <option>Movie</option>
-                    <option>Vacation</option>
-                    <option>Shopping</option>
-                    <option>Others</option>
-                    </select>
-                </div><div>
-                <button>Add Expense</button></div><hr className={classes.hr}/>
-                <div className={classes.details}>
-                    <div className={classes.Am}>Amount(Rs)</div>
-                    <div className={classes.Des}>Description</div>
-                    <div className={classes.Cat}>Category</div>
-                </div>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            </form>
-            {totalExpense > 10000 && (
-                <button className={classes.btn}onClick={() => 
-                    dispatch(ThemeAction.onThemeChange())}>
-                    Active Premium
+        <div className={classes.auth}>
+          <div >
+            {!isLogin &&<SignUp onSignUp={SignUpHandler} />}
+            {isLogin && <Login onLogin={LoginHandler} />}
+            {!isLogin && (
+                <button 
+                    className={classes.changebtn}
+                    onClick={onClickSignUpHandler}>
+                    Have an account? Login
+                </button>
+                
+            )}
+            </div>
+            <div className={classes.forgot}>
+            {isForgot && <ForgotPassword  onForgot={forgotPasswordHandler}/> }
+            {isLogin && (
+              <button 
+                className={classes.button}
+                onClick={onClickPasswordHandler}>
+                Forgot password ?  
+              </button>
+            )}
+            </div>
+            <br />
+            <br />
+            <div>
+            {isLogin && (
+                <button 
+                    className={classes.changebtn}
+                    onClick={onClickLoginHandler}>
+                    Don't have an account? Sign up
                 </button>
             )}
-            <div>
-                <ul>
-                    {Object.keys(expensesDispatched.expenses).map((expense) => {
-                    return (
-                            <li className={classes.list} key={expense}>
-                                <span style={{fontWeight: 'bold', 
-                                              marginLeft:'5%'}}>
-                                   Rs: {expensesDispatched.expenses[expense].amount}  
-                                </span>
-
-                                <span style={{
-                                    marginLeft:'15%'
-                                }}> 
-                                    {expensesDispatched.expenses[expense].description}
-                                </span>
-
-                                <span style={{
-                                    marginLeft:'25%'
-                                }}>  
-                                    { expensesDispatched.expenses[expense].category }  
-                                </span>
-                                <button  
-                                        style={{
-                                            backgroundColor: 'green',
-                                            color:'white',
-                                            border:'1px solid green',
-                                            marginLeft:'20%'
-                                        }} 
-                                        onClick={() => editExpenseHandler(expense)}>Edit
-                                    </button>
-                                    <button
-                                        style={{
-                                            backgroundColor: 'red',
-                                            marginLeft:'10px',
-                                            color:'white',
-                                            border: '1px solid red'
-                                        }} 
-                                        onClick={() => deleteExpenseHandler(expense)}>Delete
-                                    </button>
-                            </li>
-                        )
-                    })}
-                </ul>
             </div>
-        </Fragment>
-    );
-
+        </div>
+    )
 };
 
-export default Expense;
+export default Authentication;
