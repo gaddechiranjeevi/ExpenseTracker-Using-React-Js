804f0aa88e6de8b1f506253d729dddd745078a8c
gaddechiranjeevi <gaddechiru@gmail.com>
2023-02-14 20:01:16 +0530

    reducer

---
+++ src/store/auth-reducer.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialIdToken = localStorage.getItem('idToken');
+const initialEmail = localStorage.getItem("email");
+const initalAuthState = {
+    idToken: initialIdToken,
+    email: initialEmail,
+    isLogin: !!initialIdToken
+};
+
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initalAuthState,
+    reducers: {
+        login(state, action) {
+            state.idToken = action.payload.idToken;
+            state.email = action.payload.email;
+            state.isLogin = action.payload.idToken;
+            localStorage.setItem('idToken', state.idToken);
+            localStorage.setItem('email', state.email);
+        },
+        logout(state) {
+            state.idToken = null;
+            state.isLogin = false;
+            localStorage.removeItem('idToken');
+            localStorage.removeItem('email');
+        }
+    }
+});
+export const authAction = authSlice.actions;
+export default authSlice.reducer;
